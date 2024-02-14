5bd555f61d9bc72b32233046a7c8a264f4ca6ab0
wkingsurin <wkingsurin@gmail.com>
2024-02-14 19:07:59 +0500

    stylized layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
-import "./App.css";
+import appStyles from "./App.module.css";
 import "normalize.css";
 
 export default function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <h2 className="logo">todos</h2>
-          <div className="new-todo">
-            <input type="text" className="input" placeholder="Write todo..." />
-            <button className="save">save</button>
+      <div className={appStyles.container}>
+        <div className={appStyles.content}>
+          <h2 className={appStyles.logo}>todos</h2>
+          <div className={appStyles.newTodo}>
+            <input
+              type="text"
+              className={appStyles.input}
+              placeholder="Write todo..."
+            />
+            <button className={appStyles.save}>save</button>
           </div>
-          <hr className="hr" />
-          <ul className="todos">
-            <li className="todo">
-              <div className="text">Wash up</div>
-              <div className="interaction">
-                <button className="complete">
+          <span className={appStyles.hr} />
+          <ul className={appStyles.todos}>
+            <li className={appStyles.todo}>
+              <div className={appStyles.textBlock}>
+                <p className={appStyles.text}>Wash up</p>
+                <span className={appStyles.textEdit}>
+                  <svg
+                    version="1.0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="512.000000pt"
+                    height="512.000000pt"
+                    viewBox="0 0 512.000000 512.000000"
+                    preserveAspectRatio="xMidYMid meet"
+                  >
+                    <g
+                      transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
+                      fill="#000000"
+                      stroke="none"
+                    >
+                      <path
+                        d="M4090 5091 c-124 -40 -156 -64 -390 -297 l-225 -224 547 -547 548
+-548 211 210 c225 224 277 290 310 394 45 146 35 280 -33 421 -38 80 -52 97
+-257 301 -230 230 -270 259 -401 294 -77 21 -241 19 -310 -4z"
+                      />
+                      <path
+                        d="M1743 2837 l-1422 -1422 -162 -650 c-119 -477 -161 -658 -155 -678 9
+-36 46 -74 81 -83 27 -7 1276 296 1325 322 14 7 661 649 1438 1426 l1412 1413
+-548 548 -547 547 -1422 -1423z"
+                      />
+                    </g>
+                  </svg>
+                </span>
+              </div>
+              <div className={appStyles.interaction}>
+                <button className={appStyles.complete + " " + appStyles.button}>
                   <svg
                     version="1.0"
                     xmlns="http://www.w3.org/2000/svg"
@@ -41,7 +74,7 @@ export default function App() {
                     </g>
                   </svg>
                 </button>
-                <button className="remove">
+                <button className={appStyles.remove + " " + appStyles.button}>
                   <svg
                     version="1.0"
                     xmlns="http://www.w3.org/2000/svg"
